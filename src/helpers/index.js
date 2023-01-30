d224a52248059ed70ec0bae8570c24110febd283
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-29 22:40:40 -0500

    Helpers: obtenerDiferenciaYear y calcularMarca

---
+++ src/helpers/index.js
@@ -0,0 +1,21 @@
+export function obtenerDiferenciaYear(year){
+    return new Date().getFullYear() - year
+}
+
+export function calcularMarca(marca){
+    let incremento;
+
+    switch(marca){
+        case "1" : incremento = 1.3
+        break;
+        case "2" : incremento = 1.15
+        break;
+        case "3" : incremento = 1.05
+        break;
+        default:
+        break;
+    }
+
+    return incremento
+}
+
