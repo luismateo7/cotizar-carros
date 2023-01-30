fc0185909521df5e74bc5e0d14a7a8704e12d1cf
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-29 22:33:01 -0500

    Importar el CotizadorProvider

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { CotizadorProvider } from './context/CotizadorProvider'
+import AppSeguros from './components/AppSeguros'
+
+function App() {
+  return (
+    <CotizadorProvider>
+      <AppSeguros />
+    </CotizadorProvider>
+  )
+}
+
+export default App
