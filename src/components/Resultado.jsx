642e0e511be3ce8755ed6f5d4cccdf35e4c6e4b7
Mateo Rodriguez <luismateor73@gmail.com>
2023-01-29 22:39:29 -0500

    Mostrar el resultado de la cotización

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,46 @@
+import { useMemo, useRef } from "react"
+import useCotizador from "../hooks/useCotizador"
+import { MARCAS, PLANES } from "../constants"
+
+export default function Resultado() {
+
+    const { resultado, datos } = useCotizador()
+    const { marca, plan, year } = datos
+    const yearRef = useRef(year)
+
+    const [ nombreMarca ] = useMemo( () =>
+        MARCAS.filter(marcas => marcas.id === Number(marca)),
+        [resultado]
+    )
+    const [ nombrePlan ] = useMemo( () =>
+        PLANES.filter( planes => planes.id === Number(plan)),
+        [ resultado ]
+    )
+
+    if(resultado === 0) return null
+
+    return (
+        <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+            <h2 className="text-gray-600 font-black text-3xl">Resumen</h2>
+            <p className="my-2">
+                <span className="font-bold">Marca: </span>
+                {nombreMarca.nombre}
+            </p>
+
+            <p className="my-2">
+                <span className="font-bold">Año del Auto: </span>
+                {yearRef.current}
+            </p>
+            
+            <p className="my-2">
+                <span className="font-bold">Plan: </span>
+                {nombrePlan.nombre}
+            </p>
+
+            <p className="my-2 text-xl">
+                <span className="font-bold">Total Cotización: </span>
+                ${resultado}
+            </p>
+        </div>
+    )
+}
